7574875b03a7c61c5b2a81b488085a8b346e5f94
DmitriyYankovskiy <dmityank@gmail.com>
2023-10-29 14:26:24 +0300

    initial_commit

---
+++ target/debug/deps/network_test-b87d69aa23f8d9d9.d
@@ -0,0 +1,5 @@
+/home/dmitriy/programming/rust/network_test/target/debug/deps/network_test-b87d69aa23f8d9d9.rmeta: src/main.rs
+
+/home/dmitriy/programming/rust/network_test/target/debug/deps/network_test-b87d69aa23f8d9d9.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/network_test-e7404b0e9b57069d.d
@@ -0,0 +1,5 @@
+/home/dmitriy/programming/rust/network_test/target/debug/deps/network_test-e7404b0e9b57069d.rmeta: src/main.rs
+
+/home/dmitriy/programming/rust/network_test/target/debug/deps/network_test-e7404b0e9b57069d.d: src/main.rs
+
+src/main.rs:
