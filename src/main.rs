50130abf2fc1d90c8d993e26ae78defaf2988323
DmitriyYankovskiy <dmityank@gmail.com>
2024-02-22 01:07:33 +0300

    sfdsf

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
-use network::Network;
+use network::{activation::SIGMOID, Network, learning::{DataSet, Data}};
 
-extern crate network;
 fn main() {
-    println!("Hello, world!");
+    let mut net = Network::new(vec![4, 5, 3, 3], 1.0, SIGMOID);
+
+    let tests = DataSet::from_table(vec![
+        (vec![0, 0, 0, 0], vec![0, 0, 0]),
+        (vec![0, 0, 0, 1], vec![0, 0, 1]),
+        (vec![0, 0, 1, 0], vec![0, 1, 0]),
+        (vec![0, 0, 1, 1], vec![0, 1, 1]),
+        (vec![0, 1, 0, 0], vec![0, 0, 1]),
+        (vec![0, 1, 0, 1], vec![0, 1, 0]),
+        (vec![0, 1, 1, 0], vec![0, 1, 1]),
+        (vec![0, 1, 1, 1], vec![1, 0, 0]),
+        (vec![1, 0, 0, 0], vec![0, 1, 0]),
+        (vec![1, 0, 0, 1], vec![0, 1, 1]),
+        (vec![1, 0, 1, 0], vec![1, 0, 0]),
+        // (vec![1, 0, 1, 1], vec![1, 0, 1]),
+        (vec![1, 1, 0, 0], vec![0, 1, 1]),
+        (vec![1, 1, 0, 1], vec![1, 0, 0]),
+        (vec![1, 1, 1, 0], vec![1, 0, 1]),
+        (vec![1, 1, 1, 1], vec![1, 1, 0]),
+    ]);
+
+    net.train(tests, 100000);
+
+    net.predict(Data::new(vec![1, 0, 1, 1])).error();
 }
