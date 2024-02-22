8996f3bcb068b149aa17081efe02c3d7981bf7e8
DmitriyYankovskiy <dmityank@gmail.com>
2024-02-23 00:22:43 +0300

    fdf

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,31 @@
 use network::{activation::SIGMOID, Network, learning::{DataSet, Data}};
 
 fn main() {
-    let mut net = Network::new(vec![4, 5, 3, 3], 1.0, SIGMOID);
+    let mut cnt = 0;
+    let mut net = Network::new(vec![5, 5, 1], 0.02, 0.3, SIGMOID);
+    //  net.load(&"network.json".to_string());
+    for i in 0..30 {
+        let tests = DataSet::from_table(vec![
+            (vec![0, 1, 0, 1, 0], vec![1]),
+            (vec![0, 1, 1, 1, 0], vec![1]),
+            (vec![0, 1, 0, 0, 0], vec![0]),
+            (vec![1, 0, 0, 0, 1], vec![1]),
+            (vec![0, 0, 0, 1, 1], vec![0]),
+            (vec![1, 0, 0, 0, 0], vec![0]),
+            (vec![1, 1, 0, 0, 1], vec![0]),
+            (vec![1, 1, 0, 1, 0], vec![0]),
+        ]);
 
-    let tests = DataSet::from_table(vec![
-        (vec![0, 0, 0, 0], vec![0, 0, 0]),
-        (vec![0, 0, 0, 1], vec![0, 0, 1]),
-        (vec![0, 0, 1, 0], vec![0, 1, 0]),
-        (vec![0, 0, 1, 1], vec![0, 1, 1]),
-        (vec![0, 1, 0, 0], vec![0, 0, 1]),
-        (vec![0, 1, 0, 1], vec![0, 1, 0]),
-        (vec![0, 1, 1, 0], vec![0, 1, 1]),
-        (vec![0, 1, 1, 1], vec![1, 0, 0]),
-        (vec![1, 0, 0, 0], vec![0, 1, 0]),
-        (vec![1, 0, 0, 1], vec![0, 1, 1]),
-        (vec![1, 0, 1, 0], vec![1, 0, 0]),
-        // (vec![1, 0, 1, 1], vec![1, 0, 1]),
-        (vec![1, 1, 0, 0], vec![0, 1, 1]),
-        (vec![1, 1, 0, 1], vec![1, 0, 0]),
-        (vec![1, 1, 1, 0], vec![1, 0, 1]),
-        (vec![1, 1, 1, 1], vec![1, 1, 0]),
-    ]);
+        net.train(tests, 1000, false);
 
-    net.train(tests, 100000);
-
-    net.predict(Data::new(vec![1, 0, 1, 1])).error();
+        let res = net.predict(Data::new(vec![1, 0, 1, 0, 1]));
+        let Data(data) = &res;
+        if data.iter().map(|x| x.round() as i32).collect::<Vec<i32>>() == vec![0] {
+            cnt += 1;
+        }
+        
+        res.view();
+    }
+    net.save(&"network.json".to_string());
+    println!("{} / {}", cnt, 100);
 }
